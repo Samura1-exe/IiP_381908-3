31b926a9afd5cad183b8fbda473acd6059fb2ece
AndrewGudkov <gudkov1999an@muil.ru>
2020-04-19 17:59:41 +0300

    laba1

---
+++ Gudkov AV/Task1/Frac.cpp
@@ -0,0 +1,103 @@
+#include "Frac.h"
+
+int found0(int c) {
+	if (c == 0) {
+		
+		return 0;
+	}
+	else return 1;
+
+
+}
+void fraction::SetChisl(int n)
+{
+	this->chisl = n;
+}
+
+void fraction::SetZnam(int n)
+{
+	this->znam = n;
+}
+
+int fraction::GetChisl()
+{
+	return this->chisl ;
+}
+
+int fraction::GetZnam()
+{
+	return this->znam;
+}
+
+int fraction::Umn(fraction drob1, fraction drob2)
+{
+	int c;
+	int d;
+	c = drob1.GetChisl() * drob2.GetChisl();
+	if (found0(c) == 0)
+	{
+		cout << "0";
+		return 0;
+	}
+	else
+	{
+		d = drob1.GetZnam() * drob2.GetZnam();
+		cout << c << '/' << d;
+	}
+	return 0;
+
+}
+
+int fraction::Del(fraction drob1, fraction drob2)
+{
+	int c;
+	int d;
+	c = drob1.GetChisl() * drob2.GetZnam();
+	if (found0(c) == 0)
+	{
+		cout << "0";
+		return 0;
+	}
+	else
+	{
+		d = drob1.GetZnam() * drob2.GetChisl();
+		cout << c << '/' << d;
+	}
+	return 0;
+}
+
+int fraction::Sum(fraction drob1, fraction drob2)
+{
+	int c;
+	int d;
+	c = drob1.GetChisl() * drob2.GetZnam() + drob1.GetZnam() * drob2.GetChisl();
+	if (found0(c) == 0)
+	{
+		cout << "0";
+		return 0;
+	}
+	else
+	{
+		d = drob1.GetZnam() * drob2.GetZnam();
+		cout << c << '/' << d;
+	}
+	return 0;
+}
+
+int fraction::Vuch(fraction drob1, fraction drob2)
+{
+	int c;
+	int d;
+	c = drob1.GetChisl() * drob2.GetZnam() - drob1.GetZnam() * drob2.GetChisl();
+	if (found0(c) == 0)
+	{
+		cout << "0";
+		return 0;
+	}
+	else
+	{
+		d = drob1.GetZnam() * drob2.GetZnam();
+		cout << c << '/' << d;
+	}
+	return 0;
+}
